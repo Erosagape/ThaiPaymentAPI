ff11f070d641c6941d95434984584b381d824256
Administrator <Administrator@DEVSERVER>
2023-03-29 19:33:46 +0700

    **Add 2C2P API Connector**

---
+++ Controllers/Test2C2PController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Mvc;
+using Jose;
+using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using JWT.Exceptions;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using ThaiPaymentAPI.Models;
+namespace ThaiPaymentAPI.Controllers
+{
+    public class Test2C2PController : Controller
+    {
+        // GET: Test2C2P
+        public ActionResult Index()
+        {
+            ViewBag.Result = "Ready";
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            ViewBag.Result = "Ready";
+            return View();
+        }
+        public ActionResult PostPaymentPRD(FormCollection data)
+        {
+            var payload = new Dictionary<string, object>()
+            {
+                {"merchantID", _2C2PConstant.PRDMerchantID},
+                {"invoiceNo", data["invoiceNo"]},
+                {"description", data["description"]},
+                {"amount", data["amount"]},
+                {"currencyCode", data["currencyCode"]}
+            };
+            string secretkey = _2C2PConstant.PRDSecretID;
+            var algorithm = new HMACSHA256Algorithm();
+            var serializer = new JsonNetSerializer();
+            var urlEncoder = new JwtBase64UrlEncoder();
+            var Encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            var jwtToken = Encoder.Encode(payload, secretkey);
+            var requestData = new Dictionary<string, string>{
+                { "payload", jwtToken}
+            };
+            var requestStr = JsonConvert.SerializeObject(requestData);
+            var client = new WebClient();
+            client.Headers.Add("Content-Type", "application/*+json");
+            client.Headers.Add("Accept", "text/plain");
+            var responseText = client.UploadString(_2C2PConstant.PRDEndpointPaymentToken, "POST", requestStr);
+            var responseObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+            var payloadString = "";
+            try
+            {
+                var responseToken = responseObj["payload"];
+                var Provider = new UtcDateTimeProvider();
+                var validator = new JwtValidator(serializer, Provider);
+                var Decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+                payloadString = Decoder.Decode(responseToken, secretkey, true);
+                var payloadObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(payloadString);
+                var paymentUrl = payloadObj["webPaymentUrl"];
+                return Redirect(paymentUrl);
+            }
+            catch (Exception e)
+            {
+                if (responseObj.Count > 0)
+                {
+                    ViewBag.Result = JsonConvert.SerializeObject(responseObj);
+                } else
+                {
+                    ViewBag.Result = e.Message;
+                }
+            }
+            return View("Index2");
+        }
+        public ActionResult PostPaymentUAT(FormCollection data)
+        {
+            var payload = new Dictionary<string, object>()
+            {
+                {"merchantID", _2C2PConstant.UATMerchantID},
+                {"invoiceNo", data["invoiceNo"]},
+                {"description", data["description"]},
+                {"amount", data["amount"]},
+                {"currencyCode", data["currencyCode"]}
+            };
+            string secretkey = _2C2PConstant.UATSecretID;
+            var algorithm = new HMACSHA256Algorithm();
+            var serializer = new JsonNetSerializer();
+            var urlEncoder = new JwtBase64UrlEncoder();
+            var Encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            var jwtToken = Encoder.Encode(payload, secretkey);
+            var requestData = new Dictionary<string, string>{
+                { "payload", jwtToken}
+            };
+            var requestStr = JsonConvert.SerializeObject(requestData);
+            var client = new WebClient();
+            client.Headers.Add("Content-Type", "application/*+json");
+            client.Headers.Add("Accept", "text/plain");
+            var responseText = client.UploadString(_2C2PConstant.UATEndpointPaymentToken, "POST", requestStr);
+            var responseObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+            var payloadString = "";
+            try                
+            {
+                var responseToken = responseObj["payload"];
+                var Provider = new UtcDateTimeProvider();
+                var validator = new JwtValidator(serializer, Provider);
+                var Decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+                payloadString = Decoder.Decode(responseToken, secretkey, true);
+                var payloadObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(payloadString);
+                var paymentUrl = payloadObj["webPaymentUrl"];
+                return Redirect(paymentUrl);
+            }
+            catch (Exception e)
+            {
+                if (responseObj.Count > 0)
+                {
+                    ViewBag.Result = JsonConvert.SerializeObject(responseObj);
+                }
+                else
+                {
+                    ViewBag.Result = e.Message;
+                }
+            }
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult PostResponse(string paymentResponse)
+        {
+            ViewBag.Result = paymentResponse;
+            return View("Result");
+        }
+        [HttpPost]
+        [ActionName("Result")]
+        public ActionResult PostResult(FormCollection data)
+        {
+            var result = "Ready";
+            if (data.Count > 0)
+            {
+                result = "";
+                for(int i=0;i<data.Count;i++)
+                {
+                    result += "\n"+ data.Keys[i].ToString() +" : " + data[i].ToString();
+                }
+            }
+            ViewBag.Result = result;
+            return View();
+        }
+        public ActionResult Result()
+        {
+            var result = Request.QueryString["paymentResponse"];
+            if (result == null)
+                result = "Ready";
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
---
+++ Models/2C2PConstant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThaiPaymentAPI.Models
+{
+    public static class _2C2PConstant
+    {
+        public static string UATMerchantID => "JT04";
+        public static string UATSecretID => "CD229682D3297390B9F66FF4020B758F4A5E625AF4992E5D75D311D6458B38E2";
+        public static string UATEndpointPaymentToken => "https://sandbox-pgw.2c2p.com/payment/4.1/paymentToken";
+        public static string PRDMerchantID => "764764000012617";
+        public static string PRDSecretID => "7A4BD6C717B46F76E1EA1BB1DC656417ACFD3D73FEA995EFE826BB37CAE0DB66";
+        public static string PRDEndpointPaymentToken => "https://pgw.2c2p.com/payment/4.1/paymentToken";
+    }
+}
