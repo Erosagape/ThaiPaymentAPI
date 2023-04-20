eeaefa12676d0556a8aa6c52ef63984abfa70b72
Administrator <Administrator@DEVSERVER>
2023-04-20 17:52:10 +0700

    Add GBPrime Payment Form

--- Controllers/GBPrimeController.cs
+++ Controllers/GBPrimeController.cs
@@ -15,18 +15,230 @@ namespace ThaiPaymentAPI.Controllers
         {
             return View();
         }
-        public ActionResult TestAPIPayment()
+        [HttpPost]
+        public ActionResult Checkout(FormCollection form)
+        {
+            TempData["OrderNo"] = form["orderNo"];
+            TempData["OrderAmt"] = form["orderAmt"];
+            TempData["OrderDetail"] = form["orderDetail"];
+            TempData["CustomerName"] = form["customerName"];
+            TempData["CustomerAddress"] = form["customerAddress"];
+            TempData["CustomerEmail"] = form["customerEmail"];
+            TempData["CustomerPhone"] = form["customerPhone"];
+            return RedirectToAction("CardPayment");
+        }
+        public ActionResult CardPayment()
+        {
+            ViewBag.OrderNo = DateTime.Now.ToString("yyyyMMddHHMMss");            
+            if (TempData["OrderNo"] != null)
+            {
+                ViewBag.OrderNo = TempData["OrderNo"];
+                TempData["OrderNo"] = ViewBag.OrderNo;
+            }
+            ViewBag.OrderAmount = 0;
+            if (TempData["OrderAmt"] != null)
+            {
+                ViewBag.OrderAmt = TempData["OrderAmt"];
+                TempData["OrderAmt"] = ViewBag.OrderAmt;
+            }
+            ViewBag.OrderDetail = "";
+            if (TempData["OrderDetail"] != null)
+            {
+                ViewBag.OrderDetail = TempData["OrderDetail"];
+                TempData["OrderDetail"] = ViewBag.OrderDetail;
+            }
+            ViewBag.CustomerName = "";
+            if (TempData["CustomerName"] != null)
+            {
+                ViewBag.CustomerName = TempData["CustomerName"];
+                TempData["CustomerName"] = ViewBag.CustomerName;
+            }
+            ViewBag.CustomerAddress = "";
+            if (TempData["CustomerAddress"] != null)
+            {
+                ViewBag.CustomerAddress = TempData["CustomerAddress"];
+                TempData["CustomerAddress"] = ViewBag.CustomerAddress;
+            }
+            ViewBag.CustomerEmail = "";
+            if (TempData["CustomerEmail"] != null)
+            {
+                ViewBag.CustomerEmail = TempData["CustomerEmail"];
+                TempData["CustomerEmail"] = ViewBag.CustomerEmail;
+            }
+            ViewBag.CustomerPhone = "";
+            if (TempData["CustomerPhone"] != null)
+            {
+                ViewBag.CustomerPhone = TempData["CustomerPhone"];
+                TempData["CustomerPhone"] = ViewBag.CustomerPhone;
+            }
+            ViewBag.Result = "Ready";
+            return View();
+        }
+
+        public ActionResult Payment()
+        {
+            return View();
+        }
+        public ActionResult TestTokenCardPayment()
+        {
+            try
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                var endpoint = Models.GBPConstants.UATEnvironmentUrl + Models.GBPConstants.TokenCreateUrl;
+                var req = WebRequest.Create(endpoint);
+                var keyBytes =  System.Text.UTF8Encoding.UTF8.GetBytes(Models.GBPConstants.UATPublicKey);                
+                var authorizeStr = String.Format("Basic {0}:", Convert.ToBase64String(keyBytes));
+                req.Method = "POST";
+                req.Headers.Add("Authorization", authorizeStr);
+
+                var payload = new Models.GBPRequestCardToken()
+                {
+                    rememberCard = false,
+                    card = new Models.GBPCardInfo
+                    {
+                        number = "5258860689905862",
+                        expirationMonth = "02",
+                        expirationYear = "25",
+                        securityCode = "950",
+                        name = "Test Card UAT"
+                    }
+                };
+                var payloadString = JsonConvert.SerializeObject(payload);
+                var payloadBytes = System.Text.Encoding.UTF8.GetBytes(payloadString);                
+                req.ContentType = "application/json";
+                req.ContentLength = payloadBytes.Length;
+
+                var stream = req.GetRequestStream();
+                stream.Write(payloadBytes, 0, payloadBytes.Length);
+                stream.Close();
+
+                var response = req.GetResponse();
+                var res = response.GetResponseStream();
+                var reader = new System.IO.StreamReader(res);
+                var responseStr = reader.ReadToEnd();
+                return Content(responseStr, "text/json");
+            } catch (Exception e)
+            {
+                return Content(e.Message, "text/html");
+            }
+        }
+        [HttpPost]
+        [ActionName("CardPayment")]
+        public ActionResult PostCardPayment(FormCollection form)
+        {
+            var paytype = form["payType"];
+            var amount = Convert.ToDouble(form["orderAmt"]);
+            var detail = form["orderDetail"];
+            var custName = form["customerName"];
+            var custEmail = form["customerEmail"];
+            var custAddress = form["customerAddress"];
+            var custTelephone = form["customerTelephone"];
+            var cardNo = form["cardNumber"];
+            var secureCode = form["secureCode"];
+            var expireMonth = form["expireMonth"];
+            var expireYear = form["expireYear"];
+            var orderNo = form["orderNo"];
+            var objToken = (paytype=="PRD"? Models.GBPManager.GetTokenResultPRD(): Models.GBPManager.GetTokenResultUAT());
+            if (objToken.resultCode.Equals("00"))
+            {
+                var tokenStr = objToken.authToken;
+                var order = new Models.GBPRequestPaymentTransaction()
+                {
+                    amount=amount,
+                    detail=detail,
+                    customerAddress=custAddress,
+                    customerName=custName,
+                    customerEmail=custEmail,
+                    customerTelephone=custTelephone,                    
+                    cardNumber=cardNo,
+                    rememberCard=false,
+                    securityCode=secureCode,
+                    expirationMonth=expireMonth,
+                    expirationYear=expireYear,
+                    otp="Y",
+                    paymentType="C",
+                    referenceNo=orderNo,
+                    responseUrl = "https://www.aih-consultant.com/backend/gbprime/result",
+                    backgroundUrl = "https://www.aih-consultant.com/backend/gbprime/result"
+                };
+                var res = Models.GBPManager.Payment(order, tokenStr,paytype);
+                if (res.resultCode.Equals("00"))
+                {
+                    return Redirect(res.redirectUrl);
+                } else
+                {
+                    ViewBag.Result = res.resultMessage;
+                }
+            }
+            else
+            {
+                ViewBag.Result =objToken.resultMessage;
+            }
+            ViewBag.Result = "Ready";
+            return View();
+        }
+        public ActionResult TestUnifiedPayment()
         {
             string content = "Testing";
-            content += "\nToken UAT : " + JsonConvert.SerializeObject(Models.GBPManager.GetTokenResultUAT());
-            content += "\nToken PRD : " + JsonConvert.SerializeObject(Models.GBPManager.GetTokenResultPRD());
+            var objTokenuat = Models.GBPManager.GetTokenResultUAT();
+            var objTokenprd = Models.GBPManager.GetTokenResultPRD();
+            content += "\nToken UAT = " + objTokenuat.resultMessage + " : " + objTokenuat.authToken;
+            content += "\nToken PRD = "+ objTokenprd.resultMessage +" : " + objTokenprd.authToken;
+            var testresponse = JsonConvert.DeserializeObject<Models.GBPResponseUnified>(Models.GBPManager.TestPayment(objTokenuat.authToken));
+            content += "\nTest Payment UAT = " + testresponse.resultMessage;
+            if (testresponse.resultCode.Equals("00"))
+            {
+                return Redirect(testresponse.redirectUrl);
+            }
             return Content(content, "text/html");
         }
         public ActionResult Token()
         {
+            //orderNo=20230417110435&orderAmount=500&transTypw=test&gbToken=d11d6282-3ad1-4567-ad79-6742208273c0&gbRememberCard=false
             ViewBag.GBPToken = Request.QueryString["gbToken"];
-            bool rememberCard = (Request.QueryString["gbRememberCard"] == "true");
+            ViewBag.TransType = Request.QueryString["transType"];
+            ViewBag.OrderNo = Request.QueryString["orderNo"];
+            ViewBag.OrderAmount = Convert.ToDouble(Request.QueryString["orderAmount"]);
+            ViewBag.RememberCard = (Request.QueryString["gbRememberCard"] == "true");
             return View();
+        }
+        [HttpPost]
+        [ActionName("Token")]
+        public ActionResult PostToken(FormCollection form)
+        {
+            var token = form["token"];
+            var transType = form["transType"];
+            var orderNo = form["orderNo"];
+            var orderAmt = Convert.ToDouble(form["orderAmount"]);
+            var rememberCard = (form["rememberCard"] == "true");
+            var obj = new Models.GBPRequestCardPayment()
+            {
+                amount = orderAmt,
+                referenceNo = orderNo,
+                card = new Dictionary<string, string>()
+                {
+                    {"token",token }
+                },
+                detail = "Test Products",
+                otp = "Y",
+                customerName = "TEST COMPANY",
+                customerEmail = "all@tawantech.co.th",
+                customerAddress = "TEST ADDRESS",
+                customerTelephone = "+66823320506",
+                backgroundUrl = "Result",
+                responseUrl = "Result"
+            };
+            var endpoint = Models.GBPConstants.UATEnvironmentUrl + Models.GBPConstants.TokenChargeUrl;
+            var requestStr = JsonConvert.SerializeObject(obj);
+            var secretkeyBytes = System.Text.Encoding.ASCII.GetBytes(Models.GBPConstants.UATSecretKey);
+
+            var client = new WebClient();
+            client.Headers.Add("Content-Type", "application/json");
+            client.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(secretkeyBytes) + ":");
+            var responseText = client.UploadString(endpoint, "POST", requestStr);
+            ViewBag.Result= responseText;
+            return View("Result");
         }
         [HttpPost]
         [ActionName("Index")]
@@ -36,6 +248,29 @@ namespace ThaiPaymentAPI.Controllers
             ViewBag.PayType = form["paytype"];
             return View("Payment");
         }
+        [HttpPost]
+        public ActionResult PostResultForm(FormCollection form)
+        {
+            string response = "";
+            foreach(var data in form)
+            {
+                response += "<br/>" + data.ToString();
+            }
+            ViewBag.Result = response;
+            return View("Result");
+        }
+        [HttpPost]
+        [ActionName("Result")]
+        public ActionResult PostResult(Models.GBPResponseCallBack data)
+        {
+            string response = data.SaveLog().error;
+            ViewBag.Result = response;
+            return View("Result");
+        }
+        public ActionResult Result()
+        {
+            ViewBag.Result = "Ready";
+            return View();
+        }
     }
-
 }
--- Models/GBPConstants.cs
+++ Models/GBPConstants.cs
@@ -9,16 +9,21 @@ namespace ThaiPaymentAPI.Models
 {
     public static class GBPConstants
     {
-        public static string PRDEnvironmentUrl => "https://api.gbprimepay.com";
-        public static string UATEnvironmentUrl => "https://api.globalprimepay.com";
+        public static string PRDEnvironmentUrl => "https://api.gbprimepay.com";               
         public static string PRDMerchantID => "gbp13738";
-        public static string UATMerchantID => "gbp10599";
         public static string PRDSecretKey => "4p6CA4qg7ebs3EgyGDqa6ZwnoB54e4m8";
         public static string PRDPublicKey => "XRhskUAmmPoaa7C89BwcRdwNVRoHahiW";
+
+        public static string UATEnvironmentUrl => "https://api.globalprimepay.com";
+        public static string UATMerchantID => "gbp10599";
         public static string UATSecretKey => "erFK008OLMwAPD2VfOrZYWvurqQ85H6J";
         public static string UATPublicKey => "GqpOORx7Iw9LvkDBsNixSaAH7rqjwuc3";
+
         public static string AuthenticationUrl => "/unified/authToken/create";
         public static string UnifiedAPIUrl => "/unified/transaction";
+        public static string TokenChargeUrl => "/v2/tokens/charge";
+        public static string Call3DSecureUrl=>"/v2/tokens/3d_secured";
+        public static string TokenCreateUrl => "/v2/tokens";
     }
     public class GBPManager
     {
@@ -48,6 +53,156 @@ namespace ThaiPaymentAPI.Models
             var responseText = client.UploadString(endpoint, "POST", requestStr);
             return JsonConvert.DeserializeObject<Models.GBPResponseToken>(responseText);
         }
+        public static GBPResponseUnified Payment(GBPRequestPaymentTransaction order,string token,string paytype)
+        {
+            var client = new WebClient();
+            client.Headers.Add("content-type", "application/json");
+            client.Headers.Add("Authorization", token);
+            var requestData = new GBPRequestUnified
+            {
+                apiType = "PC",
+                createPaymentTransactionRequest = order
+            };
+            var endpoint = Models.GBPConstants.PRDEnvironmentUrl + Models.GBPConstants.UnifiedAPIUrl;
+            if (paytype == "UAT")
+                endpoint = Models.GBPConstants.UATEnvironmentUrl + Models.GBPConstants.UnifiedAPIUrl;
+            var requestStr = JsonConvert.SerializeObject(requestData);
+            var responseText = client.UploadString(endpoint, "POST", requestStr);
+            return JsonConvert.DeserializeObject<GBPResponseUnified>(responseText);
+        }
+        public static string TestPayment(string token)
+        {
+            var client = new WebClient();
+            client.Headers.Add("content-type", "application/json");
+            client.Headers.Add("Authorization", token);
+            var payload = new GBPRequestPaymentTransaction()
+            {
+                referenceNo = DateTime.Now.ToString("yyyyMMddHHMMss"),
+                paymentType = "C",
+                amount = 500,
+                cardNumber = "5258860689905862",
+                expirationMonth = "02",
+                expirationYear = "25",
+                securityCode = "950",
+                rememberCard = false,
+                otp = "Y",
+                detail = "TEST",
+                responseUrl="https://www.aih-consultant.com/backend/gbprime/result",
+                backgroundUrl = "https://www.aih-consultant.com/backend/gbprime/result"                
+            };
+            var requestData = new GBPRequestUnified {
+                apiType ="PC",
+                createPaymentTransactionRequest =payload
+            };
+            var endpoint = Models.GBPConstants.UATEnvironmentUrl + Models.GBPConstants.UnifiedAPIUrl;
+            var requestStr = JsonConvert.SerializeObject(requestData);
+            var responseText = client.UploadString(endpoint, "POST", requestStr);
+            return responseText;
+        }
+    }
+    public class GBPResponseCallBack
+    {
+        public double amount { get; set; }
+        public string referenceNo { get; set; }
+        public string gbpReferenceNo { get; set; }
+        public string currencyCode { get; set; }
+        public string resultCode { get; set; }
+        public string paymentType { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+        public string cardNo { get; set; }
+        public string cardHolderName { get; set; }
+        public string detail { get; set; }
+        public string customerName { get; set; }
+        public string customerEmail { get; set; }
+        public string customerAddress { get; set; }
+        public string customerTelephone { get; set; }
+        public string merchantDefined1 { get; set; }
+        public string merchantDefined2 { get; set; }
+        public string merchantDefined3 { get; set; }
+        public string merchantDefined4 { get; set; }
+        public string merchantDefined5 { get; set; }
+        public string retryFlag { get; set; }
+        public ErrorResponse SaveLog()
+        {
+            return new ActionLog()
+            {
+                log_action="RES",
+                log_datetime=DateTime.Now,
+                log_error=false,
+                log_message=(this.resultCode.Equals("00") ?"Success":"Failed"),
+                log_data=Newtonsoft.Json.JsonConvert.SerializeObject(this),
+                log_source="GBPResponseCallBack",
+                log_stacktrace="",
+            }.Save();
+        }
+    }
+    public class GBPResponseUnified
+    {
+        public string apiType { get; set; }
+        public string status { get; set; }
+        public string resultCode { get; set; }
+        public string resultMessage { get; set; }
+        public string redirectUrl { get; set; }
+        public string referenceNo { get; set; }
+        public string token { get; set; }
+        public string transactionStatus { get; set; }
+        public string gbpReferenceNo { get; set; }
+        public string paymentType { get; set; }
+        public string detail { get; set; }
+        public string customerName { get; set; }
+        public string customerEmail { get; set; }
+        public string customerAddress { get; set; }
+        public string customerTelephone { get; set; }
+        public string merchantDefined1 { get; set; }
+        public string merchantDefined2 { get; set; }
+        public string merchantDefined3 { get; set; }
+        public string merchantDefined4 { get; set; }
+        public string merchantDefined5 { get; set; }
+    }
+    public class GBPRequestUnified
+    {
+        public string apiType { get; set; }
+        public GBPRequestPaymentTransaction createPaymentTransactionRequest { get; set; }
+    }
+    public class GBPRequestCardPayment
+    {
+        public double amount { get; set; }
+        public string referenceNo { get; set; }
+        public string detail { get; set; }
+        public string customerName { get; set; }
+        public string customerEmail { get; set; }
+        public string customerAddress { get; set; }
+        public string customerTelephone { get; set; }
+        public Dictionary<string,string> card { get; set; }
+        public string otp { get; set; }
+        public string responseUrl { get; set; }
+        public string backgroundUrl { get; set; }
+    }
+    public class GBPRequestPaymentTransaction
+    {
+        public string referenceNo { get; set; }
+        public string paymentType { get; set; }
+        public double amount { get; set; }
+        public string cardNumber { get; set; }
+        public string expirationMonth { get; set; }
+        public string expirationYear { get; set; }
+        public string securityCode { get; set; }
+        public string cardToken { get; set; }
+        public bool rememberCard { get; set; }
+        public string otp { get; set; }
+        public string responseUrl { get; set; }
+        public string backgroundUrl { get; set; }
+        public string detail { get; set; }
+        public string customerName { get; set; }
+        public string customerEmail { get; set; }
+        public string customerAddress { get; set; }
+        public string customerTelephone { get; set; }
+        public string merchantDefined1 { get; set; }
+        public string merchantDefined2 { get; set; }
+        public string merchantDefined3 { get; set; }
+        public string merchantDefined4 { get; set; }
+        public string merchantDefined5 { get; set; }
 
     }
     public class GBPResponseToken
@@ -56,5 +211,18 @@ namespace ThaiPaymentAPI.Models
         public string resultMessage { get; set; }
         public string authToken { get; set; }
         public int expireIn { get; set; }
+    }
+    public class GBPCardInfo
+    {
+        public string number { get; set; }
+        public string expirationMonth { get; set; }
+        public string expirationYear { get; set; }
+        public string securityCode { get; set; }
+        public string name { get; set; }
+    }
+    public class GBPRequestCardToken
+    {
+        public bool rememberCard { get; set; }
+        public GBPCardInfo card { get; set; }
     }
 }
